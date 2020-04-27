0742a7a4a9b2e19c805ea1e59ebb169731827fce
hsisco <hysensisco@gmail.com>
2020-04-26 23:10:11 -0400

    helpers. middleware, routers

--- data/seeds/00-users.js
+++ data/seeds/00-users.js
@@ -1,15 +1,15 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('generic_table').truncate()
+  return knex('users').truncate()
     .then(function () {      
     // Inserts seed entries
       return knex('users').insert([
-        {id: 1, username: 'Val', password: 'qwerty', department: 'Performative Wokeness'},
-        {id: 2, username: 'Shawn', password: 'qwerty', department: 'Toxic Masculinity'},
-        {id: 3, username: 'Chris', password: 'qwerty', department: 'Spastic Dentistry'},
-        {id: 4, username: 'Janet', password: 'qwerty', department: 'Children’s Dance Recital'},
-        {id: 5, username: 'Trevor', password: 'qwerty', department: 'Holiday Weekend at Ikea'}
+        {id: 1, name: 'Val', password: 'qwerty', department: 'Performative Wokeness'},
+        {id: 2, name: 'Shawn', password: 'qwerty', department: 'Toxic Masculinity'},
+        {id: 3, name: 'Chris', password: 'qwerty', department: 'Children’s Dance Recital'},
+        {id: 4, name: 'Janet', password: 'qwerty', department: 'Children’s Dance Recital'},
+        {id: 5, name: 'Trevor', password: 'qwerty', department: 'Performative Wokeness'}
       ]);
     });
 };
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-const authRouter = require('./auth-router');
-const usersRouter = require('./users-router');
+const authRouter = require('./auth/auth-router');
+const usersRouter = require('./users/users-router');
 
 const server = express();
 
@@ -11,8 +11,8 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('./auth-router', authRouter);
-server.use('./users-router', usersRouter);
+server.use('/auth', authRouter);
+server.use('/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send("Generic user authenticator")
